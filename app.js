2529d53bcb0e4c5d0ba8eeff523e7e4e31a9f43f
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-11-19 08:24:18 +0100

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.APP_PORT ?? 5004;
 app.use(express.json()); 
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to hell");
 };
 
 app.get("/", welcome);
@@ -17,6 +17,10 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", usersHandlers.postUser);
+app.put("/api/users/:id", usersHandlers.updateUsers);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.listen(port, (err) => {
   if (err) {
@@ -27,5 +31,3 @@ app.listen(port, (err) => {
 });
 
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", usersHandlers.postUser);
